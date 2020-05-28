6a2a98908490c94f4dec11c50cba60437ea77c54
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-05-28 14:33:16 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,15 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.trajectory = [];
+  }
+
+  display() {
+    super.display();
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var pos = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(pos);
+    }
+  }
+}
